246a9af0002235b5f932c980e55b2cdd99a0fc28
Lusaiwen <1811977648@qq.com>
2022-10-15 14:01:14 +0800

    vue3-vite

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>
+    <button @click="increase">count:{{ countRef }}</button>
+  </p>
+</template>
+<script>
+import { ref } from 'vue'
+
+function useCount() {
+  let countRef = ref(0);
+  const increase = () => {
+    countRef.value++;
+  }
+  return {
+    countRef,
+    increase
+  }
+}
+
+export default {
+  setup() {
+    return {
+      ...useCount()
+    }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+
+
+const app = createApp(App);
+console.log(app);
+app.mount('#app');
